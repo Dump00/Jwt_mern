0fc0f05ed5d4e8033778b059bbd66d51350ddbc9
cisco <sovisrushain@gmail.com>
2022-08-08 23:17:30 +0530

    connect to mongodb

--- server/index.js
+++ server/index.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose')
+import {User} from './models/user.model'
 
 app.use(cors())
 app.use(express.json())
 
-app.post('/api/register', (req,res) => {
+mongoose.connect('mongodb://localhost:27017/auth-mern')
+
+app.post('/api/register', async(req,res) => {
     console.log(req.body);
+    try {
+        await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        })
+        res.json({status: 'ok'})
+    } catch (err) {
+        res.json({status: 'error', error: "Duplicate email"})
+    }
     res.json({status: 'ok'})
 })
 
